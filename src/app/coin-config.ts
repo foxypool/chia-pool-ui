50c14c04a3e0c199d52140020410e048c18d8939
Felix Brucker <felix@felixbrucker.com>
2021-08-14 00:58:24 +0200

    Update github urls

--- src/app/coin-config.ts
+++ src/app/coin-config.ts
@@ -1,11 +1,11 @@
 const config = {
   CHIA: {
-    clientSoftwareDownloadUrl: 'https://github.com/felixbrucker/chia-blockchain',
+    clientSoftwareDownloadUrl: 'https://github.com/foxypool/chia-blockchain',
     clientSoftwareName: 'chia-blockchain',
     cliCommandPrefix: 'chia',
   },
   FLAX: {
-    clientSoftwareDownloadUrl: 'https://github.com/felixbrucker/flax-blockchain',
+    clientSoftwareDownloadUrl: 'https://github.com/foxypool/flax-blockchain',
     clientSoftwareName: 'flax-blockchain',
     cliCommandPrefix: 'flax',
   },
