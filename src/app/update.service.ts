268c94ba5fc50a21ef3186128c6eed44de484e9b
Felix Brucker <felix@felixbrucker.com>
2021-08-14 12:53:53 +0200

    Update app periodically and recover from bad states

--- src/app/update.service.ts
+++ src/app/update.service.ts
@@ -1,7 +1,9 @@
-import {Injectable} from "@angular/core";
+import {ApplicationRef, Injectable} from "@angular/core";
 import {SwUpdate} from "@angular/service-worker";
 import {ToastService} from "./toast.service";
 import {SnippetService} from "./snippet.service";
+import {first} from 'rxjs/operators';
+import {concat, interval} from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -11,14 +13,40 @@ export class UpdateService {
     swUpdate: SwUpdate,
     toastService: ToastService,
     snippetService: SnippetService,
+    appRef: ApplicationRef
   ) {
+    if (!swUpdate.isEnabled) {
+      return;
+    }
     swUpdate.available.subscribe(async () => {
       toastService.showInfoToast(snippetService.getSnippet('update-service.updating'), '', { timeOut: 2 * 1000 });
-      await Promise.all([
-        swUpdate.activateUpdate(),
-        new Promise(resolve => setTimeout(resolve, 2 * 1000)),
-      ]);
+      await new Promise(resolve => setTimeout(resolve, 2 * 1000));
+      await swUpdate.activateUpdate();
       document.location.reload();
     });
+    swUpdate.unrecoverable.subscribe(async () => {
+      await this.clearCacheStorage();
+      document.location.reload();
+    });
+    const appIsStable$ = appRef.isStable.pipe(first(isStable => isStable === true));
+    const everySixHours$ = interval(6 * 60 * 60 * 1000);
+    const everySixHoursOnceAppIsStable$ = concat(appIsStable$, everySixHours$);
+
+    everySixHoursOnceAppIsStable$.subscribe(() => swUpdate.checkForUpdate());
+  }
+
+  async clearCacheStorage(): Promise<boolean[][]> {
+    const cacheKeys = await caches.keys();
+
+    return Promise.all(
+      cacheKeys
+        .filter(cacheKey => /^(ngsw).*/.test(cacheKey))
+        .map(async cacheKey => {
+          const cache = await caches.open(cacheKey);
+          const requests = await cache.keys();
+
+          return Promise.all(requests.map(req => cache.delete(req)));
+        })
+    );
   }
 }
