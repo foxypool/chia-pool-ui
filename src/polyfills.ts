829e90740ca25de10f4d0d64b302d308287eec17
Felix Brucker <felix@felixbrucker.com>
2021-11-15 17:27:45 +0100

    Add ResizeObserver polyfill

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -81,3 +81,4 @@ import 'zone.js';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+import './polyfills/resize-observer';
