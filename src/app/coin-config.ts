392b83cd116f70b95ddd260a34bc240598ad74e0
Felix Brucker <felix@felixbrucker.com>
2022-08-20 10:12:50 +0200

    Remove stai, hddcoin and flax from meta tags

--- src/app/coin-config.ts
+++ src/app/coin-config.ts
@@ -4,25 +4,10 @@ const config = {
     hdKeyPoolPublicKeyPath: 'm/12381n/8444n/1n/0n',
     decimalPlaces: 12,
   },
-  FLAX: {
-    cliCommandPrefix: 'flax',
-    hdKeyPoolPublicKeyPath: 'm/12381n/8444n/1n/0n',
-    decimalPlaces: 12,
-  },
   CHIVES: {
     cliCommandPrefix: 'chives',
     hdKeyPoolPublicKeyPath: 'm/12381n/9699n/1n/0n',
     decimalPlaces: 8,
-  },
-  HDDCOIN: {
-    cliCommandPrefix: 'hddcoin',
-    hdKeyPoolPublicKeyPath: 'm/12381/8444/1/0',
-    decimalPlaces: 12,
-  },
-  STAI: {
-    cliCommandPrefix: 'stai',
-    hdKeyPoolPublicKeyPath: 'm/12381n/8444n/1n/0n',
-    decimalPlaces: 9,
   },
 };
 
