67dffdabdd1bdbf587faaac2f957c3461fe1fdb5
Felix Brucker <felix@felixbrucker.com>
2023-03-26 09:23:30 +0200

    Remove chives

--- src/app/coin-config.ts
+++ src/app/coin-config.ts
@@ -4,11 +4,6 @@ const config = {
     hdKeyPoolPublicKeyPath: 'm/12381n/8444n/1n/0n',
     decimalPlaces: 12,
   },
-  CHIVES: {
-    cliCommandPrefix: 'chives',
-    hdKeyPoolPublicKeyPath: 'm/12381n/9699n/1n/0n',
-    decimalPlaces: 8,
-  },
 };
 
 export interface CoinConfig {
