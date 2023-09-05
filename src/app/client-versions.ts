8991e59486839959eb57b685abfe57b455e2466a
Felix Brucker <felix@felixbrucker.com>
2023-09-05 13:10:59 +0530

    Bump recent versions and recommended minimum versions, closes #3

--- src/app/client-versions.ts
+++ src/app/client-versions.ts
@@ -1,22 +1,22 @@
 export const clientVersions = {
   chia: {
     minimum: '1.8.0',
-    recommendedMinimum: '1.8.1',
-    current: '1.8.0',
-    recent: '1.7.0',
-    outdated: '1.5.0',
+    recommendedMinimum: '2.0.0',
+    current: '2.0.0',
+    recent: '1.8.0',
+    outdated: '1.6.0',
   },
   og: {
     minimum: '1.2.0',
-    recommendedMinimum: '1.3.0',
+    recommendedMinimum: '1.4.0',
   },
   foxyFarmer: {
     minimum: '1.2.0',
-    recommendedMinimum: '1.3.0',
+    recommendedMinimum: '1.6.1',
   },
   gigahorse: {
     minimum: 10,
-    recommendedMinimum: 10,
+    recommendedMinimum: 14,
   },
   chiaCompressionAlpha: {
     minimum: '4.3',
