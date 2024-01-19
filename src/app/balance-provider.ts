7af4a012fee472b96229539d5c78e235b937b04b
Felix Brucker <felix@felixbrucker.com>
2024-01-19 18:33:46 +0700

    Use oracle in balance provider

--- src/app/balance-provider.ts
+++ src/app/balance-provider.ts
@@ -1,45 +1,30 @@
 import { Injectable } from '@angular/core'
 import axios, {AxiosInstance} from 'axios'
 import {BigNumber} from 'bignumber.js'
-import {TtlCache} from './ttl-cache'
 
 @Injectable({
   providedIn: 'root'
 })
 export class BalanceProvider {
-  private readonly cache: TtlCache = new TtlCache(29 * 60)
   private readonly client: AxiosInstance = axios.create({
-    baseURL: 'https://chia.tt/api/chia',
+    baseURL: 'https://oracle.foxypool.io/v1',
   })
 
   public constructor() {}
 
   public async getBalance(address: string): Promise<BigNumber> {
-    const cachedBalance = this.cache.get<BigNumber>(address)
-    if (cachedBalance !== undefined) {
-      return cachedBalance
-    }
-
-    const balance = await this.getBalanceFromApi(address)
-    this.cache.set(address, balance)
-
-    return balance
-  }
-
-  private async getBalanceFromApi(address: string): Promise<BigNumber> {
     try {
-      const { data } = await this.client.get<GetAddressResponse>(`blockchain/address/${address}`)
+      const { data } = await this.client.get<string>('balance', {
+        params: {
+          type: 'coin',
+          symbol: 'xch',
+          address,
+        },
+      })
 
-      return new BigNumber(data.balance)
+      return new BigNumber(data)
     } catch (err) {
       return new BigNumber(0)
     }
   }
-}
-
-interface GetAddressResponse {
-  address: string
-  txCount: number
-  balance: string
-  flag: boolean
 }
