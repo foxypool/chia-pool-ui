117d75ab145b96be64e857740550505420cd2827
Felix Brucker <felix@felixbrucker.com>
2025-02-15 22:55:17 +0900

    Remove notices and manually show warning

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -3,6 +3,7 @@ import {StatsService} from '../stats.service'
 import {SnippetService} from '../snippet.service'
 import * as moment from 'moment'
 import {Notice} from '../api/types/pool/pool-config'
+import {PoolsProvider} from '../pools.provider'
 
 @Component({
   selector: 'app-dashboard',
@@ -14,6 +15,7 @@ export class DashboardComponent  {
   constructor(
     private readonly statsService: StatsService,
     private readonly _snippetService: SnippetService,
+    private readonly poolsProvider: PoolsProvider,
   ) {}
 
   get snippetService() {
@@ -33,15 +35,11 @@ export class DashboardComponent  {
     return this.notices.filter(notice => moment(notice.date).isAfter(moment().subtract(1, 'week')))
   }
 
-  public get hasNotices(): boolean {
-    return this.noticesInTheLastWeekOrLess.length > 0
+  public get showNotices(): boolean {
+    return true
   }
 
-  public getTranslatedNoticeText(notice: Notice): string {
-    return notice.text.en
-  }
-
-  getFormattedDate(date) {
-    return moment(date).format('YYYY-MM-DD')
+  get downloadUrl(): string {
+    return this.poolsProvider.pool.downloadUrl
   }
 }
