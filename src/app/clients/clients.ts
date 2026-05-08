0c5635d5aa61c99357523f89bdcf8dd4603247b2
Felix Brucker <felix@felixbrucker.com>
2026-05-08 07:21:26 +0700

    Update recommended chia versions

--- src/app/clients/clients.ts
+++ src/app/clients/clients.ts
@@ -23,11 +23,11 @@ export const chiaClient: Client<string> = {
     localNames: [],
   },
   versions: {
-    minimum: '2.5.1',
-    recommendedMinimum: '2.5.1',
-    current: '2.5.1',
-    recent: '2.5.0',
-    outdated: '2.4.0',
+    minimum: '2.7.0',
+    recommendedMinimum: '2.7.0',
+    current: '2.7.0',
+    recent: '2.7.0',
+    outdated: '2.5.1',
   },
 }
 export const chiaOgClient: Client<string> = {
@@ -103,8 +103,8 @@ export const gigahorseClient: Client<number> = {
     localNames: ['giga'],
   },
   versions: {
-    minimum: 22,
-    recommendedMinimum: 36,
+    minimum: 36,
+    recommendedMinimum: 37,
   },
 }
 export const fastFarmerClient: Client<string> = {
